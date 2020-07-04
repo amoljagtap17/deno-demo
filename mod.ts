3141225d5e7afcea4e269afccfe848a1582c36f8
Amol Jagtap <amoljagtap17@gmail.com>
2020-07-04 14:01:42 +0530

    DENO-DEMO: Sample habitable planets code

--- mod.ts
+++ mod.ts
@@ -2,18 +2,35 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+interface IPlanet {
+  [key: string]: string;
+}
+
 const loadPlanetsData = async () => {
   const path = join(".", "kepler_exoplanets_nasa.csv");
 
   const file = await Deno.open(path);
   const bufReader = new BufReader(file);
+
   const data = await parse(bufReader, {
     header: true,
     comment: "#",
   });
+
   Deno.close(file.rid);
 
-  console.log(data);
+  const planets = (data as IPlanet[]).filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" && planetaryRadius > 0.5 &&
+      planetaryRadius < 1.5 && stellarMass > 0.78 && stellarMass < 1.04 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01;
+  });
+
+  return planets;
 };
 
-await loadPlanetsData();
+const newPlanets = await loadPlanetsData();
+console.log(`${newPlanets.length} habitable planets found!`);
